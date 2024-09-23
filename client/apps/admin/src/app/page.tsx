938ae2da0da24847877f085d358207965ab6db9d
adityaa72 <adityah1357908642@gmail.com>
2024-09-23 16:57:47 +0530

    feat: admin panel

---
+++ client/apps/admin/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <p className="text-4xl">Hii 👋 there, this is a mcart admin panel</p>
+    </div>
+  );
+}
