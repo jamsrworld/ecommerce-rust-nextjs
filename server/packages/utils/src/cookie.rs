71d2120c5eff23a2ec3e0f385e74d1bd16ee37d0
adityaa72 <adityah1357908642@gmail.com>
2024-10-29 23:31:42 +0530

    feat: server docker

--- server/packages/utils/src/cookie.rs
+++ server/packages/utils/src/cookie.rs
@@ -2,12 +2,12 @@ use actix_web::cookie::{ time::Duration, Cookie, SameSite };
 
 // pub fn create_cookie<'a>(name: impl Into<String>, value: impl Into<String>) -> Cookie<'a> {
 pub fn create_cookie(name: impl Into<String>, value: impl Into<String>) -> Cookie<'static> {
-    let domain = "mcart.dev";
+    let domain = "mcart-rust-nextjs.vercel.app";
     Cookie::build(name.into(), value.into())
         .path("/")
         .http_only(true)
         .secure(true)
-        .same_site(SameSite::Lax)
+        .same_site(SameSite::None)
         .domain(domain)
         .max_age(Duration::new(60 * 60 * 24, 0))
         .finish()
