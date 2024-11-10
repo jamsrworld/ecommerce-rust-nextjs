5a8cadd764e143b34e7034f69dc3352128a94418
adityaa72 <adityah1357908642@gmail.com>
2024-11-11 01:10:36 +0530

    feat: jamsr-ui new version

--- client/apps/www/src/app/(auth)/logout/route.ts
+++ client/apps/www/src/app/(auth)/logout/route.ts
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 export const GET = async () => {
   const cookiesStore = await cookies();
+  cookiesStore.delete({ name: "x-session", domain: ".jamsrworld.com" });
   cookiesStore.delete("x-session");
   redirect("/");
 };
