d5d5cd806a1fc597a1b6ab40de5d7f061c679819
adityaa72 <adityah1357908642@gmail.com>
2024-11-21 16:14:30 +0530

    fix: logout

--- client/apps/www/src/app/(auth)/logout/actions.ts
+++ client/apps/www/src/app/(auth)/logout/actions.ts
@@ -3,9 +3,18 @@
 import { env } from "@/env";
 import { cookies } from "next/headers";
 
+const domain = (_url: string) => {
+  const { hostname } = new URL(_url);
+  const parts = hostname.split(".");
+  if (parts.length > 1) {
+    // Combine the second-level domain and top-level domain
+    return `.${parts.at(-2)}.${parts.at(-1)}`;
+  }
+  return undefined;
+};
+
 export const logout = async () => {
   const cookiesStore = await cookies();
-  const host = env.NEXT_PUBLIC_APP_URL;
-  const url = new URL(host);
-  cookiesStore.delete({ name: "x-session", domain: url.hostname });
+  const url = env.NEXT_PUBLIC_APP_URL;
+  cookiesStore.delete({ name: "x-session", domain: domain(url) });
 };
