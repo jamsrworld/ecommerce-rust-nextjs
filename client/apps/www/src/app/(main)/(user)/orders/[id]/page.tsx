32f11bd62734ce6212780e5cad44c280f76f97f7
adityaa72 <adityah1357908642@gmail.com>
2024-11-22 17:02:46 +0530

    feat: order view page

--- client/apps/www/src/app/(main)/(user)/orders/[id]/page.tsx
+++ client/apps/www/src/app/(main)/(user)/orders/[id]/page.tsx
@@ -1,64 +1,33 @@
+import { getOrder } from "@/client";
+import { APP_ROUTES } from "@/config/routes";
+import { authedClient } from "@/utils/authed-client";
 import { Divider, Typography } from "@jamsr-ui/react";
+import { NextImage, NextLink } from "@repo/components/next";
 import { fPrice } from "@repo/utils/number";
-import Image from "next/image";
-import Img1 from "../assets/1.webp";
-import Img2 from "../assets/2.webp";
-import Img3 from "../assets/3.webp";
+import { getProductThumbnail } from "@repo/utils/product";
+import { fDateTime } from "@repo/utils/time";
 
-// Enum for order status
-enum OrderStatus {
-  Pending = "Pending",
-  Shipped = "Shipped",
-  Delivered = "Delivered",
-  Cancelled = "Cancelled",
-}
-
-// Example data from the image
-const order = {
-  id: "0876432",
-  status: OrderStatus.Pending,
-  products: [
-    {
-      title: "Ipad Pro 11",
-      specifications: ["Space gray", "256GB"],
-      price: 999.0,
-      quantity: 1,
-      thumbnail: Img1,
-    },
-    {
-      title: "Airpods Pro",
-      specifications: ["White"],
-      price: 249.0,
-      quantity: 1,
-      thumbnail: Img2,
-    },
-    {
-      title: 'Macbook Pro 14"',
-      specifications: ["Space gray", "16GB ", "1TB"],
-      price: 1999.0,
-      quantity: 1,
-      thumbnail: Img3,
-    },
-  ],
-  delivery: {
-    address: "2626 Rockwell Lane Apt. 120",
-    city: "Overland Park",
-    country: "US",
-    phoneNumber: "913-664-1217",
-    method: "Free (12 days)",
-    estimatedDelivery: "Mar 12, 2024",
-  },
-  summary: {
-    subtotal: 2914.2,
-    discount: 323.8,
-    deliveryCost: 0.0,
-    tax: 291.42,
-    total: 3205.62,
-  },
+type Props = {
+  params: Promise<{ id: string }>;
 };
 
-const Page = () => {
-  const { delivery, products, id, status, summary } = order;
+const Page = async (props: Props) => {
+  const { id } = await props.params;
+  const fetchClient = await authedClient();
+  const { data } = await getOrder({
+    client: fetchClient,
+    path: {
+      id,
+    },
+  });
+  if (!data) return <div>Order not found</div>;
+  const { order, product } = data;
+  const { title, price, images, size, color, brand } = product;
+  const thumbnail = getProductThumbnail(images);
+  const { quantity, address, paymentMethod, status, createdAt, updatedAt } =
+    order;
+  const specifications = [size, color, brand];
+  const productUrl = APP_ROUTES.products.view(product.id, product.slug);
   return (
     <div>
       <Typography
@@ -70,77 +39,75 @@ const Page = () => {
       <Divider className="my-8" />
       <section>
         <div className="flex flex-col gap-6">
-          {products.map((item, idx) => {
-            const { title, price, quantity, thumbnail, specifications } = item;
-            return (
-              <div
-                key={idx}
-                className="flex gap-4"
+          <div className="flex gap-4">
+            <NextLink href={productUrl}>
+              <NextImage
+                image={thumbnail}
+                alt={title}
+                className="size-14 rounded object-cover"
+              />
+            </NextLink>
+            <div className="flex grow flex-col gap-1">
+              <Typography
+                as={NextLink}
+                href={productUrl}
               >
-                <Image
-                  src={thumbnail}
-                  alt={title}
-                  className="size-14 rounded"
-                />
-                <div className="flex grow flex-col gap-1">
-                  <Typography as="p">{title}</Typography>
-                  <div className="flex gap-1 text-foreground-secondary">
-                    {specifications.map((item) => {
-                      return (
-                        <Typography
-                          key={item}
-                          as="p"
-                          variant="paragraph2"
-                        >
-                          {item}
-                        </Typography>
-                      );
-                    })}
-                  </div>
-                </div>
-                <div className="flex flex-col gap-1 text-right">
-                  <Typography as="p">{fPrice(price)}</Typography>
-                  <Typography
-                    variant="caption"
-                    as="p"
-                    className="text-foreground-secondary"
-                  >
-                    Qty: {quantity}
-                  </Typography>
-                </div>
+                {title}
+              </Typography>
+              <div className="flex gap-1 divide-x text-foreground-secondary">
+                {specifications.map((item) => {
+                  return (
+                    <Typography
+                      key={item}
+                      as="p"
+                      variant="paragraph2"
+                      className="pl-1"
+                    >
+                      {item}
+                    </Typography>
+                  );
+                })}
               </div>
-            );
-          })}
+            </div>
+            <div className="flex flex-col gap-1 text-right">
+              <Typography as="p">{fPrice(price)}</Typography>
+              <Typography
+                variant="caption"
+                as="p"
+                className="text-foreground-secondary"
+              >
+                Qty: {quantity}
+              </Typography>
+            </div>
+          </div>
         </div>
       </section>
       <Divider className="my-8" />
       <section className="flex flex-col gap-4">
-        <Typography
-          as="p"
-          variant="h6"
-        >
-          Delivery
-        </Typography>
         <div className="grid grid-cols-2 text-sm">
           <div className="flex flex-col gap-1">
             <Typography
               className="text-foreground-tertiary"
               as="p"
             >
-              Address
+              Delivery Address
             </Typography>
             <div>
-              <Typography as="p">{delivery.address}</Typography>
               <Typography as="p">
-                {delivery.city}, {delivery.country}
+                {address.first_name} {address.last_name}
               </Typography>
-              <Typography as="p">{delivery.phoneNumber}</Typography>
+              <Typography as="p">{address.full_address}</Typography>
+              <Typography as="p">
+                {address.postal_code}, {address.city}, {address.state}
+              </Typography>
+              <Typography as="p">{address.phone_number}</Typography>
+              <Typography as="p">{address.landmark}</Typography>
             </div>
             <Typography
               as="p"
               className="mt-4 text-success"
             >
-              Estimated Delivery: {delivery.estimatedDelivery}
+              Estimated Delivery: {fDateTime(updatedAt)}
             </Typography>
           </div>
           <div className="flex flex-col gap-1">
@@ -148,10 +115,10 @@ const Page = () => {
               className="text-foreground-tertiary"
               as="p"
             >
-              Delivery Method
+              Payment Method
             </Typography>
             <div>
-              <Typography as="p">{delivery.method}</Typography>
+              <Typography as="p">{paymentMethod}</Typography>
             </div>
           </div>
         </div>
@@ -167,19 +134,19 @@ const Page = () => {
         <div className="flex flex-col gap-2 text-sm">
           <div className="flex justify-between">
             <Typography as="p">Subtotal</Typography>
-            <Typography as="p">{fPrice(summary.subtotal)}</Typography>
+            <Typography as="p">{fPrice(price)}</Typography>
           </div>
           <div className="flex justify-between text-foreground-tertiary">
             <Typography as="p">Discount</Typography>
-            <Typography as="p">{fPrice(summary.discount)}</Typography>
+            <Typography as="p">{fPrice(0)}</Typography>
           </div>
           <div className="flex justify-between text-foreground-tertiary">
             <Typography as="p">Delivery</Typography>
-            <Typography as="p">{fPrice(summary.deliveryCost)}</Typography>
+            <Typography as="p">{fPrice(0)}</Typography>
           </div>
           <div className="flex justify-between">
             <Typography as="p">Tax</Typography>
-            <Typography as="p">{fPrice(summary.tax)}</Typography>
+            <Typography as="p">{fPrice(0)}</Typography>
           </div>
           <div className="flex justify-between">
             <Typography
@@ -194,7 +161,7 @@ const Page = () => {
               as="p"
               className="font-medium"
             >
-              {fPrice(summary.total)}
+              {fPrice(price)}
             </Typography>
           </div>
         </div>
