32a896d86958b1486271cdcfe1809b6cbd2c8466
adityaa72 <adityah1357908642@gmail.com>
2024-11-26 14:06:41 +0530

    feat: new home page

--- client/apps/www/src/app/(main)/(home)/page.tsx
+++ client/apps/www/src/app/(main)/(home)/page.tsx
@@ -3,21 +3,23 @@ import { Banner2Section } from "./components/banner2";
 import { Banner3Section } from "./components/banner3";
 import { Banner4Section } from "./components/banner4";
 import { Banner5Section } from "./components/banner5";
+import { Banner7Section } from "./components/banner7";
+import { Banner8Section } from "./components/banner8";
 import { CarouselSection } from "./components/carousel";
 import { CarouselShortsSection } from "./components/carousel-shorts";
 import { CarouselTShirtsSection } from "./components/carousel-tshirts";
-import { CategoriesSection } from "./components/categories";
 import { CouponsSection } from "./components/coupons";
 import { HeroSection } from "./components/hero";
 
 const Page = () => {
   return (
-    <div className="container flex max-w-screen-4xl flex-col gap-24 md:px-6 md:pb-6">
+    <div className="container flex max-w-screen-4xl flex-col gap-16 md:px-6 md:pb-6">
       <div className="flex flex-col gap-2">
         <HeroSection />
         <CouponsSection />
+        <Banner7Section />
       </div>
-      <CategoriesSection />
+      <Banner8Section />
       <div>
         <Banner4Section />
         <Banner3Section />
